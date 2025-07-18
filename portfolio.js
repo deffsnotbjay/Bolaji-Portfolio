4812129462c2614883237f18c297de3af436fc6a
Ologunde Omobolaji <omobolajiologunde@gmail.com>
2025-07-18 21:01:39 +0100

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,17 +1,30 @@
+    setTimeout(function() {
+      const loader = document.getElementById("loading-screen");
+      loader.classList.add("fade-out");
+
+      setTimeout(function() {
+        loader.style.display = "none";
+        document.getElementById("main-content").style.display = "block";
+      }, 1000); // matches the fade-out duration
+    }, 5000);
+
+//  setTimeout(function() {
+//     document.getElementById("loading-screen").style.display = "none";
+//     document.getElementById("main-content").style.display = "block";
+//   }, 5000);
+
 document.addEventListener('DOMContentLoaded', function() {
     const hamburgerMenu = document.getElementById('hamburgerMenu');
     const mobileMenuContent = document.getElementById('mobileMenuContent');
     const navbarContainer = document.querySelector('.navbar');
 
     hamburgerMenu.addEventListener('click', function() {
-        // Toggle 'active' class on hamburger icon for animation
+        
         hamburgerMenu.classList.toggle('active');
-        // Toggle 'menu-open' on container to control visibility of the mobile menu content
         navbarContainer.classList.toggle('menu-open');
     });
 
-    // Close menu if a link is clicked (for better mobile UX)
-    // Now querying links within the mobileMenuContent
+  
     mobileMenuContent.querySelectorAll('.navbar-links-mobile a').forEach(link => {
         link.addEventListener('click', () => {
             if (navbarContainer.classList.contains('menu-open')) {
@@ -21,6 +34,8 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
 
  const heroText = document.getElementById("herotext");
     const headline = document.getElementById("headline");
